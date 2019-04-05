337b5a15c5c0ad79bbea443ccf5c52b002f6d572
sdwpearson <sdwpearson@gmail.com>
2019-04-05 08:56:16 -0400

    [PHY1610] Final touching up

--- plot_scaling.m
+++ plot_scaling.m
@@ -23,11 +23,33 @@ diff_data =[1 133.701;
             14 19.330;
             15 18.707;
             16 18.042];
+walk_data =[1 81.910;
+            2 45.399;
+            3 32.988;
+            4 25.272;
+            5 20.980;
+            6 18.803;
+            7 17.777;
+            8 16.084;
+            9 14.467;
+            10 13.338;	
+            11 12.519;
+            12 11.907;
+            13 11.890;
+            14 11.588;
+            15 11.661;	
+            16 10.967];
+
         
 Ts_diff = diff_data(1,2);
 Tp_diff = diff_data(16,2);
 S_diff = Ts_diff/Tp_diff
 f_diff = (1-S_diff/16)/(S_diff-S_diff/16)
+
+Ts_walk = walk_data(1,2);
+Tp_walk = walk_data(16,2);
+S_walk = Ts_walk/Tp_walk
+f_walk = (1-S_walk/16)/(S_walk-S_walk/16)
         
 % Plot diffring scaling
 figure;
@@ -35,4 +57,11 @@ plot(diff_data(:,1), diff_data(:,2))
 ylabel('Walltime (s)')
 xlabel('OMP NUM THREADS')
 title('Scaling analysis for diffring')
+        
+% Plot walkring scaling
+figure;
+plot(walk_data(:,1), walk_data(:,2))
+ylabel('Walltime (s)')
+xlabel('OMP NUM THREADS')
+title('Scaling analysis for walkring')
 
